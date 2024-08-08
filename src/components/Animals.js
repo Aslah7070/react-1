b1cc651946583bc009e746ebbb493235c0cbc2b9
aslah-c <aslah.c77@gmail.com>
2024-08-08 19:01:59 +0530

    task-1

---
+++ src/components/Animals.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+function Display(){
+    return (
+        <div>
+            <h1>Animals</h1>
+            <img src="https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?cs=srgb&dl=pexels-pixabay-47547.jpg&fm=jpg" alt="404" />
+             <img src="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/8eb6ed26-b894-4e19-8960-4db9ed5cc5c5/width=450/00530-471790193-_lora_Cute%20Animals_1_Cute%20Animals%20-%20fantastic%20creature%20anime%20simple%20tiny%20cute%20flaphy%20and%20soft%20pretty%20little%20animal%20ultra%20realist.jpeg" alt="" />
+             <img src="https://images1.wionews.com/images/wion/900x1600/2023/3/20/1679292563827_Cutemain.jpg" alt="" />
+             <img src="https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/txwgyofrqqgo0jxud41i.jpg" alt="" />
+             <img src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?cs=srgb&dl=pexels-chevanon-1108099.jpg&fm=jpg" alt="" />
+             <img src="https://cdn.pixabay.com/photo/2020/02/05/15/19/meerkat-4821484_640.jpg" alt="" />
+             <img src="https://i.ytimg.com/vi/kj1-rR3udNs/sddefault.jpg" alt="" />
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUCwgvSTfhaWwkStRx7bGdVbn4nqY130wdfw&s" alt="" />
+             <img src="https://cdn.britannica.com/81/151381-050-D1DAF9CC/puppy-basket.jpg" alt="" />
+        </div>
+    )
+}
+export default Display
---
+++ src/components/Birds.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+function Birds() {
+  return (
+    <div>
+      <h1>Birds</h1>
+      <img src="https://i.etsystatic.com/27578520/r/il/b347ab/4477751760/il_fullxfull.4477751760_sr5r.jpg" alt="404" />
+      <img src="https://i.etsystatic.com/27578520/r/il/b347ab/4477751760/il_fullxfull.4477751760_sr5r.jpg" alt="404" />
+      <img src="https://i.ytimg.com/vi/MjEaniGTrx0/maxresdefault.jpg" alt="404" />
+      <img src="https://i.ytimg.com/vi/MjEaniGTrx0/maxresdefault.jpg" alt="404" />
+      <img src="https://i.ytimg.com/vi/MjEaniGTrx0/maxresdefault.jpg" alt="404" />
+      <img src="https://i.ytimg.com/vi/MjEaniGTrx0/maxresdefault.jpg" alt="404" />
+      <img src="https://preview.redd.it/i0kp88xwjuf71.png?width=466&format=png&auto=webp&s=e3bdb92ab543db0bd3e50d55f8efa90f9bb99abd" alt="404" />
+      <img src="https://i.pinimg.com/736x/83/f9/84/83f984002b32b906fe4841a3ea4d6728.jpg" alt="404" />
+      <img src="https://i.pinimg.com/736x/83/f9/84/83f984002b32b906fe4841a3ea4d6728.jpg" alt="404" />
+    </div>
+  )
+}
+
+export default Birds
